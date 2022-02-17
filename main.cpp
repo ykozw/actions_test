aa85f859e1f853a49d5fb1b2e9521b4439d12239
Yuki Ozawa <yuki.ozawa.1985@gmail.com>
2022-02-17 15:41:27 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <cstdio>
 #include <embree3/rtcore.h>
 #include <embree3/rtcore_ray.h>
+#include <tbb/parallel_for.h>
 
 int main (int argc, char*argv[])
 { 
   RTCDevice device = rtcNewDevice(0);
+  tbb::parallel_for( tbb::blocked_range<int>(0,128),
+                       [&](tbb::blocked_range<int> r)
+    {
+        for (int i=r.begin(); i<r.end(); ++i)
+        {
+            puts("test");
+        }
+    });
   puts("test");
 }
